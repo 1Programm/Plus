d85e7facf4fafccd5dda80e26b497875b6921420
Programm0 <1Pro2gra3mm45@web.de>
2021-04-03 23:36:26 +0200

    Adding goh to engine api

--- plus-engine-api/src/main/java/com/programm/projects/plus/engine/api/AbstractEngine.java
+++ plus-engine-api/src/main/java/com/programm/projects/plus/engine/api/AbstractEngine.java
@@ -1,19 +1,41 @@
 package com.programm.projects.plus.engine.api;
 
 import com.programm.projects.core.lifecycle.AbstractObservableLifecycle;
+import com.programm.projects.plus.goh.api.IGameObjectHandler;
 
 public abstract class AbstractEngine extends AbstractObservableLifecycle implements IEngine {
 
     protected IRunLoop runLoop;
+    protected IGameObjectHandler goh;
 
-    protected abstract void update();
+    protected void update(){
+        goh.update();
+    }
 
     @Override
     public void setRunLoop(IRunLoop runLoop) {
+        if(this.runLoop != null){
+            removeLifecycle(this.runLoop);
+        }
+
         this.runLoop = runLoop;
-        addToChain(runLoop);
+        addLifecycle(runLoop);
         runLoop.init(this::update);
     }
 
+    @Override
+    public void setGOH(IGameObjectHandler goh) {
+        if(this.goh != null){
+            removeLifecycle(this.goh);
+        }
 
+        this.goh = goh;
+        addLifecycle(goh);
+        //goh.init();
+    }
+
+    @Override
+    public IGameObjectHandler getGOH() {
+        return goh;
+    }
 }
--- plus-engine-api/src/main/java/com/programm/projects/plus/engine/api/IEngine.java
+++ plus-engine-api/src/main/java/com/programm/projects/plus/engine/api/IEngine.java
@@ -1,9 +1,15 @@
 package com.programm.projects.plus.engine.api;
 
 import com.programm.projects.core.lifecycle.IChainableLifecycle;
+import com.programm.projects.plus.goh.api.IGameObjectHandler;
 
 public interface IEngine extends IChainableLifecycle {
 
     void setRunLoop(IRunLoop runLoop);
+    void setGOH(IGameObjectHandler goh);
+
+
+    //Temporary - replace by Initializer - System
+    IGameObjectHandler getGOH();
 
 }
