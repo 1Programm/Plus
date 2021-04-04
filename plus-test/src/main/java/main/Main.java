98da47742d66d6af62744950e97f7ca7b001af2f
Programm0 <1Pro2gra3mm45@web.de>
2021-04-04 12:59:31 +0200

    Simplest Test scene

--- plus-test/src/main/java/main/Main.java
+++ plus-test/src/main/java/main/Main.java
@@ -1,34 +1,14 @@
 package main;
 
-import com.programm.projects.core.GameObject;
 import com.programm.projects.plus.engine.api.IEngine;
-import com.programm.projects.plus.goh.api.IGameObjectHandler;
-import com.programm.projects.plus.renderer.api.events.WindowCloseEvent;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
 
-    private static void onWindowClose(WindowCloseEvent e){
-        log.info("Window Close!!!!!!!");
-    }
-
     public static void main(String[] args) throws Exception{
         IEngine engine = new SimpleEngine();
-        engine.events().listenFor(WindowCloseEvent.class, Main::onWindowClose);
-        IGameObjectHandler goh = engine.getGOH();
-
-        goh.addObject(GameObject.create()
-            .setPosition(100, 100)
-            .setSize(32, 32)
-        .build());
-
-
         engine.startup();
-
-//        Thread.sleep(5000);
-//
-//        engine.shutdown();
     }
 
 }
