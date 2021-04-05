a06bd725369cbeb4cfd26096ec92dc4b55fe9c3f
Programm0 <1Pro2gra3mm45@web.de>
2021-04-05 13:37:11 +0200

    Testing Super Event listening

--- plus-test/src/main/java/main/Main.java
+++ plus-test/src/main/java/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
+import com.programm.projects.core.events.IEvent;
 import com.programm.projects.plus.engine.api.IEngine;
+import com.programm.projects.plus.engine.api.events.EngineEvent;
 import com.programm.projects.plus.engine.api.events.EngineShutdownEvent;
 import com.programm.projects.plus.engine.api.events.EngineStartupEvent;
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +18,15 @@ public class Main {
         log.info("ON STOP");
     }
 
+    private static void onEngine(EngineEvent e){
+        log.info("{}", e);
+    }
+
     public static void main(String[] args) throws Exception{
         IEngine engine = new SimpleEngine();
         engine.events().listenFor(EngineStartupEvent.class, Main::onStart);
         engine.events().listenFor(EngineShutdownEvent.class, Main::onStop);
+        engine.events().listenFor(EngineEvent.class, Main::onEngine);
         engine.startup();
     }
 
