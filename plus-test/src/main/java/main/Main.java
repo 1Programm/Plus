d3cf57251d7b1e22361ed6cacab45f2a2f18bcf5
Programm0 <1Pro2gra3mm45@web.de>
2021-04-05 13:14:01 +0200

    Testing listening for Engine Events

--- plus-test/src/main/java/main/Main.java
+++ plus-test/src/main/java/main/Main.java
@@ -1,13 +1,25 @@
 package main;
 
 import com.programm.projects.plus.engine.api.IEngine;
+import com.programm.projects.plus.engine.api.events.EngineShutdownEvent;
+import com.programm.projects.plus.engine.api.events.EngineStartupEvent;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
 
+    private static void onStart(EngineStartupEvent e){
+        log.info("ON START");
+    }
+
+    private static void onStop(EngineShutdownEvent e){
+        log.info("ON STOP");
+    }
+
     public static void main(String[] args) throws Exception{
         IEngine engine = new SimpleEngine();
+        engine.events().listenFor(EngineStartupEvent.class, Main::onStart);
+        engine.events().listenFor(EngineShutdownEvent.class, Main::onStop);
         engine.startup();
     }
 
