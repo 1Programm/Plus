830679436dda9635ba3a46fb21edb59bf6cf22b1
Programm0 <1Pro2gra3mm45@web.de>
2021-04-05 17:43:54 +0200

    Testing new Swing renderer

--- plus-test/src/main/java/main/Main.java
+++ plus-test/src/main/java/main/Main.java
@@ -3,14 +3,22 @@ package main;
 import com.programm.projects.core.GameObject;
 import com.programm.projects.core.IComponent;
 import com.programm.projects.core.IGameContext;
+import com.programm.projects.core.IUpdatableComponent;
+import com.programm.projects.core.components.ColorMaterial;
+import com.programm.projects.core.components.Mover;
 import com.programm.projects.plus.engine.api.IEngine;
 import com.programm.projects.plus.goh.api.IGameObjectHandler;
+import com.programm.projects.plus.renderer.swing.components.SwingShape;
 import lombok.extern.slf4j.Slf4j;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.List;
 
 @Slf4j
 public class Main {
 
-    private static IComponent moveComponent = new IComponent() {
+    private static IComponent moveComponent = new IUpdatableComponent() {
         @Override
         public void init() {
 
@@ -19,20 +27,24 @@ public class Main {
         @Override
         public void update(IGameContext context) {
             GameObject obj = context.getObject();
-            obj.getPosition().add(1f, 0);
+
+            Mover mover = obj.getComponent(Mover.class);
+            mover.move(1, 0.3f);
         }
     };
 
     public static void main(String[] args) throws Exception{
         IEngine engine = new SimpleEngine();
         IGameObjectHandler goh = engine.getGOH();
-        goh.addObject(
-                GameObject.create()
+        goh.addObject(GameObject.create()
                 .setPosition(100, 100)
-                .setSize(32, 32)
+                .setScale(4, 4)
                 .add(moveComponent)
+                .add(new ColorMaterial(Color.WHITE))
+                .add(new SwingShape(new Rectangle2D.Float(-16, -16, 16, 16)))
                 .build()
         );
+
         engine.startup();
     }
 
