7dc91dcaca98aa6c7c63396116b5f85a2de883a8
Programm0 <1Pro2gra3mm45@web.de>
2021-04-05 17:43:18 +0200

    Adding basic shape support for swing renderer

--- plus-renderer-swing/src/main/java/com/programm/projects/plus/renderer/swing/SwingCanvas.java
+++ plus-renderer-swing/src/main/java/com/programm/projects/plus/renderer/swing/SwingCanvas.java
@@ -2,6 +2,9 @@ package com.programm.projects.plus.renderer.swing;
 
 import com.programm.projects.core.GameObject;
 import com.programm.projects.core.IObjectBatch;
+import com.programm.projects.core.components.ColorMaterial;
+import com.programm.projects.core.components.Transform;
+import com.programm.projects.plus.renderer.swing.components.SwingShape;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
@@ -21,14 +24,34 @@ class SwingCanvas extends Canvas {
         g.setColor(backgroundColor);
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        g.setColor(Color.BLACK);
-        for(GameObject obj : renderableBatch){
-            int x = (int)obj.getPosition().getX();
-            int y = (int)obj.getPosition().getY();
-            int width = (int)obj.getSize().getX();
-            int height = (int)obj.getSize().getY();
+        Graphics2D g2d = (Graphics2D)g;
 
-            g.drawRect(x, y, width, height);
+        g2d.setColor(Color.BLACK);
+        for(GameObject obj : renderableBatch){
+            Transform transform = obj.getTransform();
+            int x = (int)transform.getX();
+            int y = (int)transform.getY();
+            float scaleX = transform.getXScale();
+            float scaleY = transform.getYScale();
+
+            g2d.translate(x, y);
+            g2d.scale(scaleX, scaleY);
+
+
+            ColorMaterial colorMaterial = obj.getComponent(ColorMaterial.class);
+
+            if(colorMaterial != null){
+                g.setColor(colorMaterial.getColor());
+            }
+
+            SwingShape shape = obj.getComponent(SwingShape.class);
+
+            if(shape != null){
+                g2d.draw(shape.getShape());
+            }
+
+            g2d.scale(-scaleX, -scaleY);
+            g2d.translate(-x, -y);
         }
 
         g.dispose();
