2bc1539679d4a2b4390340956ad66535b8871a2d
Programm0 <1Pro2gra3mm45@web.de>
2021-06-06 16:38:25 +0200

    Test Image rendering

--- plus-test/src/main/java/main/Main.java
+++ plus-test/src/main/java/main/Main.java
@@ -1,16 +1,16 @@
 package main;
 
+import com.programm.projects.plus.collision.api.Collider;
 import com.programm.projects.plus.components.basic.KeyboardController;
-import com.programm.projects.plus.components.basic.VelocityRotator;
 import com.programm.projects.plus.components.basic.events.ShutdownOnKeyPress;
 import com.programm.projects.plus.core.GameObject;
 import com.programm.projects.plus.core.components.Camera;
-import com.programm.projects.plus.core.exceptions.PlusException;
 import com.programm.projects.plus.engine.api.IEngine;
 import com.programm.projects.plus.engine.api.Scene;
 import com.programm.projects.plus.engine.simple.SimpleEngine;
 import com.programm.projects.plus.goh.api.IObjectConsumer;
 import com.programm.projects.plus.renderer.api.components.ColorMaterial;
+import com.programm.projects.plus.renderer.api.components.ImageMaterial;
 import com.programm.projects.plus.renderer.api.components.Model;
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,34 +31,34 @@ public class Main extends Scene {
     @Override
     protected void initScene(IObjectConsumer objects) {
         objects.add(GameObject.create()
-                .setPosition(0, 0)
+                .setPosition(130, 150)
                 .add(new KeyboardController(100))
+                .add(new Collider())
                 .add(new Camera())
-                .add(new ColorMaterial(Color.BLUE))
+                .add(new ImageMaterial("f:/test.png"))
                 .add(Model.Rectangle(32, 32))
-                .add(new VelocityRotator(1))
                 .build());
 
         objects.add(GameObject.create()
-                .add(new LoggerComponent("Tick", 1000))
-                .add(new TimedException(1000 * 10, new PlusException("Timed Exception after 10 Seconds.")))
+                .setPosition(-100, 140)
                 .add(new ColorMaterial(Color.BLACK))
-                .add(Model.Line(0, 0, 100, 10))
+                .add(new Collider())
+                .add(Model.Line(0, 0, 100, 160))
                 .build());
 
         objects.add(GameObject.create()
-                .setPosition(200, 200)
-                .setScale(1, 1)
+                .setPosition(230, 200)
                 .add(new ColorMaterial(Color.BLACK, Color.RED))
                 .add(Model.Rectangle(100, 100))
+                .add(new Collider())
                 .build());
 
-        objects.add(GameObject.create()
-                .setPosition(200, 200)
-                .setScale(3, 1)
-                .add(new ColorMaterial(Color.BLACK))
-                .add(Model.Circle(50))
-                .build());
+//        objects.add(GameObject.create()
+//                .setPosition(0, 0)
+//                .setScale(1, 1)
+//                .add(new ColorMaterial(Color.BLACK))
+//                .add(Model.Circle(500))
+//                .build());
     }
 
 
